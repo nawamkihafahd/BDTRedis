cc0b0e7c03b5c06b883ec2eda24b6bde3bdb3cc2
Dhafa Hikmawan <32931992+nawamkihafahd@users.noreply.github.com>
2019-11-26 07:14:53 +0700

    Commit

---
+++ Vagrant/redis/provision/allhosts.sh
@@ -0,0 +1,9 @@
+
+sudo cp /vagrant/sources/sources.list /etc/apt/
+
+# Update Repository
+sudo apt-get update -y
+# sudo apt-get upgrade -y
+#sudo apt install redis-server
+
+#sudo apt install redis-sentinel redis-redisearch
